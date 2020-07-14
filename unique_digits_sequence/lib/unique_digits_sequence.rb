a16ce5d91ea903baf6949a9c3d8956db2d17001a
Jed Stanton <jedstanton@hotmail.com>
2020-07-14 17:20:33 +0100

    First rspec test setup

---
+++ unique_digits_sequence/lib/unique_digits_sequence.rb
@@ -0,0 +1,2 @@
+def next_unique_digit
+end 
---
+++ unique_digits_sequence/spec/unique_digits_sequence_spec.rb
@@ -0,0 +1,7 @@
+require "unique_digits_sequence"
+
+describe "next_unique_digit" do
+  it "Returns next unique digit" do
+    expect(next_unique_digit(0)).to eq(1)
+  end
+end 
