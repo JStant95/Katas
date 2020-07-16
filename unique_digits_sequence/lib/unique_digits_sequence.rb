6f0c569468ec2a27a1ab268be3af27679c4b281c
Jed Stanton <jedstanton@hotmail.com>
2020-07-16 14:14:22 +0100

    New method - includes digit added

--- unique_digits_sequence/lib/unique_digits_sequence.rb
+++ unique_digits_sequence/lib/unique_digits_sequence.rb
@@ -2,10 +2,20 @@ def next_unique_digit(num)
   sequence = [0]
   i = 0
 
-  (num).times do
+  num.times do
     i.next
+    string_last_num = sequence.last.to_s
     sequence.push(num)
   end
 
  sequence.pop
+end
+
+def includes_digit(last_num, current_num)
+  last_num.each_char do |dig|
+    if current_num.include?(dig)
+      return true
+    end
+  end
+  false
 end
--- unique_digits_sequence/spec/unique_digits_sequence_spec.rb
+++ unique_digits_sequence/spec/unique_digits_sequence_spec.rb
@@ -17,7 +17,25 @@ describe "next_unique_digit" do
     expect(next_unique_digit(10)).to eq(10)
   end
 
-  it "Returns 11th unique digit" do
-    expect(next_unique_digit(11)).to eq(22)
+  # it "Returns 11th unique digit" do
+  #   expect(next_unique_digit(11)).to eq(22)
+  # end
+end
+
+describe "includes_digit" do
+  it "Returns true current number does not contain any digits from last number" do
+    expect(includes_digit("5", "5")).to eq(true)
+  end
+
+  it "Returns false for non matching digits" do
+    expect(includes_digit("5", "6")).to eq(false)
+  end
+
+  it "Can deal with a number with more than 1 digit" do
+    expect(includes_digit("26", "6")).to eq(true)
+  end
+
+  it "Can deal with two numbers with more than 1 digit" do
+    expect(includes_digit("26", "16")).to eq(true)
   end
 end
